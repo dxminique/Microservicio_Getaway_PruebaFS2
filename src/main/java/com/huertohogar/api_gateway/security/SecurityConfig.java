5b2de9b62a43ef2f6b61bbc3340a852280275a91
dxminique <do.cofre@duocuc.cl>
2025-11-19 16:24:15 -0300

    se volvio a arreglar el securityconfig

--- src/main/java/com/huertohogar/api_gateway/security/SecurityConfig.java
+++ src/main/java/com/huertohogar/api_gateway/security/SecurityConfig.java
@@ -2,19 +2,21 @@ package com.huertohogar.api_gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
                 );
 
         return http.build();
