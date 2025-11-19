3707839f789aaf1bbac6d9a1dcf75e1d653b5124
dxminique <do.cofre@duocuc.cl>
2025-11-19 00:10:34 -0300

    se hicieron algunos cambios

---
+++ src/main/java/com/huertohogar/api_gateway/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.huertohogar.api_gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/auth/login").permitAll()
+                        .anyExchange().authenticated()             // todo lo demás sí
+                )
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .build();
+    }
+}
