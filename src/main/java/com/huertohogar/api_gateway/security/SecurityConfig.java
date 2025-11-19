72aeda15d6160b5d3fd287b2249032e8e519caeb
dxminique <do.cofre@duocuc.cl>
2025-11-19 16:11:15 -0300

    se hicieron algunosc ambio en el security cconfig

--- src/main/java/com/huertohogar/api_gateway/security/SecurityConfig.java
+++ src/main/java/com/huertohogar/api_gateway/security/SecurityConfig.java
@@ -2,22 +2,22 @@ package com.huertohogar.api_gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(auth -> auth
-                        .pathMatchers("/auth/login").permitAll()
-                        .anyExchange().authenticated()             // todo lo demás sí
-                )
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 }
+
